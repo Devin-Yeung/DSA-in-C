71d9111878f2379977126ea173008450a0bdc5f8
Devin Yeung <devin.cg.yeung@outlook.com>
2022-01-17 23:43:34 +0800

    Add: Simple implementation of Linked-Stack

---
+++ Stack/Linked-Stack/stack.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "stack.h"
+
+
+void InitStack (Stack * S){
+    S -> top = NULL;
+    S -> size = 0;
+    return;
+}
+
+
+void push(int val, Stack * S){
+    // set up elem
+    Elem * pnew = (Elem*)malloc(sizeof(Elem));
+    pnew -> val = val;
+
+    // set the next of new element
+    pnew -> next = S -> top;
+    // reset the top
+    S -> top = pnew;
+    S -> size += 1;
+    return;
+}
+
+
+bool pop(int * val, Stack * S){
+    // check if stack if empty
+    if(S -> size == 0){
+        return 0;
+    }
+    // copy the top of stack
+    *val = S -> top -> val;
+    // store the original top
+    Elem * ptmp = S -> top;
+    // reset the top of stack
+    S -> top = S -> top -> next;
+    free(ptmp);
+    S -> size -= 1;
+    return 1;
+}
+
+
+// print out the stack from the top
+void printStack(Stack * S){
+    Elem * pscan = S -> top;
+    printf("TOP - ");
+    while(pscan != NULL){
+        printf("%d - ",pscan -> val);
+        pscan = pscan -> next;
+    }
+    printf("BOTTOM\n");
+    return;
+}
+
+int main(void){
+    Stack S;
+    int tmp;
+    InitStack(&S);
+    push(1,&S);
+    // push(3,&S);
+    // push(5,&S);
+    printStack(&S);
+    pop(&tmp,&S);
+    if(pop(&tmp,&S) == 0){
+        fprintf(stderr,"Nothing to pop\n");
+        fflush(stderr);
+    }
+    printStack(&S);
+    return 0;
+}
---
+++ Stack/Linked-Stack/stack.h
@@ -0,0 +1,26 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+
+
+typedef struct Elem {
+    int val;
+    struct Elem * next;
+} Elem;
+
+
+
+typedef struct Stack {
+    Elem * top;
+    int size;
+} Stack;
+
+
+
+void InitStack (Stack * S);
+void push(int val, Stack * S);
+bool pop(int * val, Stack * S);
+
+
+
+#endif
